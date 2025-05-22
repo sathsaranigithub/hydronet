906bf4e90392c63c7a855dcce56db2fb35f41887
D.S.Sathsarani <98662905+sathsaranigithub@users.noreply.github.com>
2025-05-22 14:48:08 +0530

    BB
    BB

--- lib/Screens/dashboard.dart
+++ lib/Screens/dashboard.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:lettus/Utils/appColors.dart';
+import 'package:lettus/Utils/appfonts.dart';
+import 'package:lettus/Widgets/label.dart';
 import 'package:provider/provider.dart';
 
 import 'package:lettus/Providers/provider.dart';
@@ -34,11 +37,11 @@ class _DashboardState extends State<Dashboard> {
         body: Column(
           children: [
             DashboardTopCard(
-                    onTap: () {
-                      Validations.showWinningDialog(context);
-                    },
-                  ),
-          Expanded(
+              onTap: () {
+                Validations.showWinningDialog(context);
+              },
+            ),
+            Expanded(
               child: SingleChildScrollView(
                 child: Column(
                   children: [
@@ -48,10 +51,83 @@ class _DashboardState extends State<Dashboard> {
                       onTap: () {
                         final formProvider =
                             Provider.of<MyModel>(context, listen: false);
-              
+
                         formProvider.updateEmail(userem.email);
-                        NavigationUtils.frontNavigation(context, PedictionPage());
+                        NavigationUtils.frontNavigation(
+                            context, PedictionPage());
                       },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Label(
+                                  hintText: "Lettuce Colour:Low green",
+                                  textColor: AppColors.green1,
+                                  fontSize: AppFonts.font14,
+                                  fontFamily: AppFonts.lemonada,
+                                  fontWeight: FontWeight.w500),
+                            ],
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          color: AppColors.white1,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: AppColors.green1,
+                            width: 2,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.2),
+                              offset: Offset(0, 4),
+                              blurRadius: 6.0,
+                              spreadRadius: 0.0,
+                            ),
+                          ],
+                        ),
+                        width: (MediaQuery.of(context).size.width - 16),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0,right: 8.0),
+                      child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Label(
+                                  hintText: "Lettuce weight:23kg",
+                                  textColor: AppColors.green1,
+                                  fontSize: AppFonts.font14,
+                                  fontFamily: AppFonts.lemonada,
+                                  fontWeight: FontWeight.w500),
+                            ],
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          color: AppColors.white1,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: AppColors.green1,
+                            width: 2,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.2),
+                              offset: Offset(0, 4),
+                              blurRadius: 6.0,
+                              spreadRadius: 0.0,
+                            ),
+                          ],
+                        ),
+                        width: (MediaQuery.of(context).size.width - 16),
+                      ),
                     ),
                     DashboardCard(
                       labelText: "SECOND component",
--- lib/Screens/yieldTable.dart
+++ lib/Screens/yieldTable.dart
@@ -110,3 +110,4 @@ class _YieldTableState extends State<YieldTable> {
     }
   }
 }
+
